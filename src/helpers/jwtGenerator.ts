ba1e860255fb5049cbd18b0a36c8c4f38e06deba
allpha23 <allpha23@gmail.com>
2022-06-11 06:03:18 -0300

    fix: (req 3 erro) ?variavel de ambiente

--- src/helpers/jwtGenerator.ts
+++ src/helpers/jwtGenerator.ts
@@ -1,12 +1,9 @@
 import jwt = require('jsonwebtoken');
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const jwtConfig = {
   expiresIn: '15d',
 };
 
-const SECRET = process.env.JWT_SECRET || '';
+const SECRET = 'batman';
 
 export = (data = {}) => jwt.sign({ data }, SECRET, jwtConfig);
